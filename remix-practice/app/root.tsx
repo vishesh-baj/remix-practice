968ca4d0f318b595faf50807d84395e2be05be71
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-01-28 11:03:54 +0530

    added layout and tailwindcss config

--- remix-practice/app/root.tsx
+++ remix-practice/app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ReactNode } from "react";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -23,11 +25,26 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Layout>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </Layout>
       </body>
     </html>
   );
 }
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <nav className="px-10 pt-5">
+        <Link to="/" prefetch="intent" className="text-2xl font-semibold">
+          Movie <span className="text-teal-500">DB</span>
+        </Link>
+      </nav>
+      <main>{children}</main>
+    </>
+  );
+};
