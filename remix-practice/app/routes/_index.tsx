81bf14f1938e9c453390fae8d0401603cd0c9607
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-01-31 19:05:31 +0530

    added styles to index.txs

--- remix-practice/app/routes/_index.tsx
+++ remix-practice/app/routes/_index.tsx
@@ -12,5 +12,16 @@ export const links: LinksFunction = () => [
 ];
 
 export default function Index() {
-  return <div>Hello from the index</div>;
+  return (
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen px-4 md:px-8 ">
+        <div className="mb-10 md:mb-16">
+          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+            Top Trending Movies
+          </h2>
+        </div>
+        <div className="grid gap-4 sm:Grid-cols-2 md:gap-6 lg:grid-cols-3 cl:grid-cols-4 xl:gap-8"></div>
+      </div>
+    </div>
+  );
 }
